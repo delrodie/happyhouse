7c69b8d1eec6bf3d76c7470ea572737399684373
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-06-09 01:33:11 +0000

    Deploy

--- deploy.php
+++ deploy.php
@@ -3,20 +3,31 @@ namespace Deployer;
 
 require 'recipe/symfony.php';
 
-// Config
+// Configuration générale
+set('application', 'Happy HOuse');
+set('repository', 'git@github.com:delrodie/happyhouse.git');
+set('allow_anonymous_stats', false);
 
-set('repository', 'https://github.com/delrodie/happyhouse.git');
+// Configuration des serveurs de déploiement
+host('access962326531.webspace-data.io')
+//    ->hostname('https://happyhouse2.esthetera.com')
+//    ->user('u112236248')
+//    ->identityFile('~/.ssh/id_rsa')
+    ->set('remote_user', 'u112236248')
+    ->set('deploy_path', '/happyhouse/v1');
 
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
+// Tâches personnalisées
+task('build', function () {
+    run('cd {{release_path}} && yarn install && yarn encore production');
+});
 
-// Hosts
+// Exécution des tâches lors du déploiement
+after('deploy:shared', 'build');
 
-host('https://happyhouse2.esthetera.com')
-    ->set('remote_user', 'deployer')
-    ->set('deploy_path', '~/happyhouse');
+// Exécution des tâches Symfony lors du déploiement
+task('database:migrate', function () {
+    run('{{/usr/bin/php8.1-cli}} {{release_path}}/bin/console doctrine:schema:update --force');
+})->desc('Execute database migrations');
 
-// Hooks
-
-after('deploy:failed', 'deploy:unlock');
+// Exécution des tâches Symfony lors du déploiement
+after('deploy:symlink', 'database:migrate');
